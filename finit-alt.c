1843a0e66fd71b85f5c941ebad7b922de86695ad
Claudio Matsuoka <cmatsuoka@gmail.com>
2008-09-11 16:45:19 -0300

    Don't close descriptors on kernel "quiet" flag
    (Asus legacy). Closing them prevents fsck output when booting quietly.
    
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- finit-alt.c
+++ finit-alt.c
@@ -249,14 +249,8 @@ int main()
 	 */
 	if ((f = fopen("/proc/cmdline", "r")) != NULL) {
 		fgets(line, LINE_SIZE, f);
-		if (strstr(line, "quiet")) {
-			close(0);
-			close(1);
-			close(2);
-		}
-		if ((strstr(line, "finit_debug"))) {
+		if ((strstr(line, "finit_debug")))
 			debug = 1;
-		}
 		fclose(f);
 	}
 
