7a3408c17329339df570c42da4c30786373fbe4f
Tobias Waldekranz <tobias@waldekranz.com>
2016-01-05 12:48:31 +0100

    Optionally skip stripping binaries during install-exec
    One may wish to build and install with debug symbols intact, so allow
    overriding the arguments sent to install.

--- common.mk
+++ common.mk
@@ -1,13 +1,16 @@
 # Top directory for building complete system, fall back to this directory
 ROOTDIR    ?= $(TOPDIR)
 
+# Strip binaries unless user explicitly disables it
+STRIPARGS ?= -s --strip-program=$(CROSS)strip -m 0755
+
 # Some junk files we always want to be removed when doing a make clean.
 JUNK        = *~ *.bak *.map .*.d *.d DEADJOE semantic.cache *.gdb *.elf core core.*
 MAKE       := @$(MAKE)
 MAKEFLAGS   = --no-print-directory --silent
 CHECK      := cppcheck $(CPPFLAGS) --quiet --enable=all
 INSTALL    := install --backup=off
-STRIPINST  := $(INSTALL) -s --strip-program=$(CROSS)strip -m 0755
+STRIPINST  := $(INSTALL) $(STRIPARGS)
 ARFLAGS    := crus
 
 export libdir plugindir incdir ROOTDIR CPPFLAGS LDFLAGS LDLIBS STATIC
