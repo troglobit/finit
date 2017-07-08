d34ee62f7758a2ad36089670fdca592f1a68c0ce
Joachim Nilsson <troglobit@gmail.com>
2017-07-08 15:32:24 +0200

    Update copyright years
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/tty.c
+++ src/tty.c
@@ -1,7 +1,7 @@
 /* Finit TTY handling
  *
- * Copyright (c) 2013  Mattias Walström <lazzer@gmail.com>
- * Copyright (c) 2013  Joachim Nilsson <troglobit@gmail.com>
+ * Copyright (c) 2013       Mattias Walström <lazzer@gmail.com>
+ * Copyright (c) 2013-2017  Joachim Nilsson <troglobit@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
