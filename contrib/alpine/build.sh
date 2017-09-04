b45a37ca47a10d5758f339c7bd84811175770ba8
Joachim Nilsson <troglobit@gmail.com>
2017-09-04 21:20:32 +0200

    Add simple build script for targeting Alpine
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

---
+++ contrib/alpine/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+
+if [ ! -f configure -a -f autogen.sh ]; then
+   echo "Building from GIT, creating configure script ..."
+   ./autogen.sh
+fi
+
+PKG_CONFIG_LIBDIR=/usr/local/lib/pkgconfig ./configure				\
+		 --enable-rw-rootfs            --enable-progress		\
+                 --enable-x11-common-plugin					\
+		 --enable-alsa-utils-plugin    --enable-inetd-echo-plugin	\
+		 --enable-inetd-chargen-plugin --enable-inetd-daytime-plugin	\
+		 --enable-inetd-discard-plugin --enable-inetd-time-plugin	\
+		 --with-heading="Alpine Linux 3.6" --with-hostname=alpine
+
+make
