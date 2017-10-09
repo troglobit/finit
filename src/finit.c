2d9c147e0e05f3668c11733304e8593df62ab0fb
Joachim Nilsson <troglobit@gmail.com>
2017-10-09 09:03:58 +0200

    Minor, fix comment within comment GCC warning
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/finit.c
+++ src/finit.c
@@ -363,8 +363,8 @@ int main(int argc, char* argv[])
 	}
 
 	/*
-	 * Parse /etc/finit.conf and all /etc/finit.d/*.conf to figure
-	 * out how to bootstrap the system.
+	 * Parse /etc/finit.conf and all *.conf in /etc/finit.d/ to
+	 * figure out how to bootstrap the system.
 	 */
 	conf_parse_config();
 
