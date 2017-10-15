troglobit/finit

commit 907595d9edf8aa9eb05fcb8b56ba755926aa7b93

getty.c: Ignore signals like SIGINT, SIGHUP
Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

src/getty.c
@@ -87,7 +87,8 @@ static void stty(int fd, speed_t speed, int noclear)
 	term.c_iflag      = ICRNL|IXON|IXOFF;
 	term.c_oflag      = OPOST|ONLCR;
 	term.c_cflag     |= CS8|CREAD|HUPCL|CBAUDEX;
-	term.c_lflag     |= ISIG|ICANON|ECHO|ECHOE|ECHOK|ECHOKE;
+	term.c_lflag     |= ICANON|ECHO|ECHOE|ECHOK|ECHOKE;
+	term.c_lflag     &= ~ISIG;
 
 	/* Reset special characters to defaults */
 	term.c_cc[VINTR]  = CTRL('C');
@@ -320,13 +321,17 @@ int getty(char *tty, char *baud, char *term, int noclear, char *user)
 		warn("Failed TIOCSCTTY");
 
 	/*
-	 * Don't let QUIT dump core.
+	 * Ignore a few signals, needed to prevent Ctrl-C at login:
+	 * prompt and to prevent QUIT from dumping core.
 	 */
 	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-	sa.sa_handler = _exit;
+	sa.sa_flags   = SA_RESTART;
+	sa.sa_handler = SIG_IGN;
+	sigaction(SIGHUP,  &sa, NULL);
+	sigaction(SIGINT,  &sa, NULL);
 	sigaction(SIGQUIT, &sa, NULL);
 
+	/* Set up TTY */
 	stty(fd, speed, noclear);
 	close(fd);
 	utmp_set_login(tty, NULL);

commit cf60cdd9c9a15165ccac726c421d843122765bf5

Fix termios regression on PPC introduced in 907595d
For some reason PPC is much more sensitive than ARM when it comes to
termios changes.  This patch fixes a regression in Ctrl-C handling
after login introduced in 907595d when blocking of Ctrl-C was added
to prevent breaking the login-prompt.

Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

src/getty.c
@@ -88,8 +88,7 @@ static void stty(int fd, speed_t speed)
 	term.c_iflag      = ICRNL|IXON|IXOFF;
 	term.c_oflag      = OPOST|ONLCR;
 	term.c_cflag     |= CS8|CREAD|HUPCL|CBAUDEX;
-	term.c_lflag     |= ICANON|ECHO|ECHOE|ECHOK|ECHOKE;
-	term.c_lflag     &= ~ISIG;
+	term.c_lflag     |= ICANON|ISIG|ECHO|ECHOE|ECHOK|ECHOKE;
 
 	/* Reset special characters to defaults */
 	term.c_cc[VINTR]  = CTRL('C');