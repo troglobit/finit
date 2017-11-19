troglobit/finit

commit a6290da2101b65a987d509e7e5a3777a3297d137

screen_init(): Verify that STDOUT is a TTY
Before we call screen_init(), which in turn calls libite:initscr(),
which in turn does a lot of tcgetattr() and termios to STDOUT to figure
out the size, we need to check that we're not backgrounded, or don't
actually have a TTY.  Problem reported by @wkz

Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

src/util.c
@@ -75,6 +75,9 @@ char *sanitize(char *arg, size_t len)
 
 void screen_init(void)
 {
+	if (!isatty(STDOUT_FILENO))
+		return
+
 	initscr(&screen_rows, &screen_cols);
 }
 

commit 6a2911223da73b7a2a144e63e7bac5594c79a0e1

Fix screen_init() regression introduced in a6290da
A simple missing semicolon caused initscr() to not be called properly.

Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

src/util.c
@@ -76,7 +76,7 @@ char *sanitize(char *arg, size_t len)
 void screen_init(void)
 {
 	if (!isatty(STDOUT_FILENO))
-		return
+		return;
 
 	initscr(&screen_rows, &screen_cols);
 }