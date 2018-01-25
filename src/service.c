troglobit/finit

commit 2c904b5b443fe4c62f318c350571ae59a6c85f0e

service_unregister(): Also call inetd_del() for inetd services
Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

src/service.c
@@ -850,6 +850,18 @@ recreate:
 
 void service_unregister(svc_t *svc)
 {
+	if (!svc)
+		return;
+
+	if (svc_is_inetd(svc)) {
+		if (svc_is_busy(svc->inetd.svc)) {
+			svc_unblock(svc->inetd.svc);
+			service_step(svc->inetd.svc);
+		}
+
+		inetd_del(&svc->inetd);
+	}
+
 	svc_del(svc);
 }
 
@@ -970,10 +982,6 @@ restart:
 	case SVC_DONE_STATE:
 #ifdef INETD_ENABLED
 		if (svc_is_inetd_conn(svc)) {
-			if (svc_is_busy(svc->inetd.svc)) {
-				svc_unblock(svc->inetd.svc);
-				service_step(svc->inetd.svc);
-			}
 			service_unregister(svc);
 			return -1;
 		}

src/inetd.c
@@ -346,8 +346,10 @@ void inetd_stop(inetd_t *inetd)
 		_d("Stopping %s socket watcher ...", inetd->svc->cmd);
 		uev_io_stop(&inetd->watcher);
 
-		/* For dgram inetd services we block the parent SVC
-		 * and halt the watcher, so don't close the socket! */
+		/*
+		 * For dgram inetd services we block the parent SVC
+		 * and halt the watcher, so don't close the socket!
+		 */
 		if (!svc_is_busy(inetd->svc)) {
 			_d("Shutting down inet socket %d ...", inetd->watcher.fd);
 			close(inetd->watcher.fd);

commit 2dc7de3aa21c4ccbac2762b8d183da1788bf27c0

Make sure to unblock inetd UDP svc when connection terminates
When an UDP inetd connection terminates we must make sure to unblock the
parent, which is (currently) blocked during the transaction.

This patch is a fix to a regression introduced in 2c904b5, for v3.1

Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

src/service.c
@@ -870,12 +870,12 @@ void service_unregister(svc_t *svc)
 	if (!svc_is_inetd_conn(svc))
 		service_stop(svc);
 
-	if (svc_is_inetd(svc)) {
+	if (!svc_is_inetd(svc)) {
 		if (svc_is_busy(svc->inetd.svc)) {
 			svc_unblock(svc->inetd.svc);
 			service_step(svc->inetd.svc);
 		}
-
+	} else {
 #ifdef INETD_ENABLED
 		inetd_del(&svc->inetd);
 #endif