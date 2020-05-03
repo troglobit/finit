9eb8e8dd99a98d7e80eeaedaa1c768ba45ac5ab1
Joachim Nilsson <troglobit@gmail.com>
2020-05-03 12:00:58 +0200

    Only track built-in watchdogd, not external ones
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/api.c
+++ src/api.c
@@ -317,8 +317,7 @@ static void api_cb(uev_t *w, void *arg, int events)
 			}
 
 			_e("Request to hand-over wdog ... to PID %d", rq.runlevel);
-			svc = svc_find_by_pid(rq.runlevel);
-			if (!svc) {
+			if (!svc_find_by_pid(rq.runlevel)) {
 				logit(LOG_ERR, "Cannot find PID %d, not registered.", rq.runlevel);
 				break;
 			}
@@ -332,7 +331,6 @@ static void api_cb(uev_t *w, void *arg, int events)
 					wdog->runlevels = 0;
 				}
 			}
-			wdog = svc;
 			break;
 
 		case INIT_CMD_SVC_ITER:
