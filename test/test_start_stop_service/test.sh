6f8c24a8d3edc5068a40d0996d6ca63eb59da633
Jacques de Laval <jacques@de-laval.se>
2021-02-22 16:48:53 +0100

    test: Only grep for specific child
    Signed-off-by: Jacques de Laval <jacques@de-laval.se>

--- test/test_start_stop_service/test.sh
+++ test/test_start_stop_service/test.sh
@@ -3,7 +3,7 @@
 set -eu
 
 assert_num_children() {
-    assert "$1 services are running" "$(texec pgrep -P 1 | wc -l)" -eq "$1"
+    assert "$1 services are running" "$(texec pgrep -P 1 "$2" | wc -l)" -eq "$1"
 }
 
 # Test
@@ -15,7 +15,7 @@ texec sh -c "echo 'service [2345] kill:20 log /bin/service.sh' > $FINIT_CONF"
 say 'Reload Finit'
 texec kill -SIGHUP 1
 
-retry 'assert_num_children 1'
+retry 'assert_num_children 1 service.sh'
 
 say 'Remove the service'
 texec sh -c "echo > $FINIT_CONF"
@@ -23,7 +23,7 @@ texec sh -c "echo > $FINIT_CONF"
 say 'Reload Finit'
 texec kill -SIGHUP 1
 
-retry 'assert_num_children 0'
+retry 'assert_num_children 0 service.sh'
 
 # Teardown
 rm -f test_root/bin/service.sh
