cde580ffce6993e933f2e1fda49ae467dc7e61cb
Jacques de Laval <jacques@de-laval.se>
2021-02-25 11:18:12 +0100

    test: Unify setup-root.sh for make check and make distcheck
    Signed-off-by: Jacques de Laval <jacques@de-laval.se>

--- test/setup-root.sh
+++ test/setup-root.sh
@@ -2,25 +2,19 @@
 
 set -eu
 
-# shellcheck disable=SC2154
-if [ ! -f "$srcdir"/../config.status ]; then
-    "$srcdir"/../configure --prefix=/
-    make
-    make DESTDIR="$(pwd)/test-root/" install
-
+if [ ! -d test-root ]; then
     mkdir -p test-root/bin
+    # shellcheck disable=SC2154
     cp "${srcdir}/test-root/bin/chrootsetup.sh" test-root/bin
     cp "${srcdir}/test-root/busybox-x86_64.md5" test-root/
     cp "${srcdir}/test-root/Makefile" test-root/
-    make -C test-root/
-    config_h='./config.h'
-    echo "export TESTENV_PATH=/bin:/sbin" > test.env
-else
-    make -C ../ DESTDIR="$(pwd)/test-root/" install
-    FINITBIN=../../src/finit make -C test-root/
-    config_h='../config.h'
-    echo "export TESTENV_PATH=/bin:/sbin:$PREFIX/sbin:$PREFIX/bin" > test.env
 fi
 
-echo "export FINIT_CONF=$(grep FINIT_CONF $config_h | cut -d' ' -f3 | cut -d'"' -f2)" >> test.env
-echo "export FINIT_RCSD=$(grep FINIT_RCSD $config_h | cut -d' ' -f3 | cut -d'"' -f2)" >> test.env
+# shellcheck disable=SC2154
+make -C "$top_builddir" DESTDIR="$(pwd)/test-root/" install
+FINITBIN="$(pwd)/$top_builddir/src/finit" make -C test-root/  # DESTDIR="$(pwd)/test-root/"
+config_h="$top_builddir/config.h"
+echo "export TESTENV_PATH=/bin:/sbin:$PREFIX/sbin:$PREFIX/bin" > test.env
+
+echo "export FINIT_CONF=$(grep FINIT_CONF "$config_h" | cut -d' ' -f3 | cut -d'"' -f2)" >> test.env
+echo "export FINIT_RCSD=$(grep FINIT_RCSD "$config_h" | cut -d' ' -f3 | cut -d'"' -f2)" >> test.env
