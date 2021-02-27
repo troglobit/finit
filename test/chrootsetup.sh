4aa17a84ab05108f7d0dd475b8347a60515032c8
Jacques de Laval <jacques@de-laval.se>
2021-02-27 13:53:55 +0100

    test: Minor clean up
    Signed-off-by: Jacques de Laval <jacques@de-laval.se>

--- test/chrootsetup.sh
+++ test/chrootsetup.sh
@@ -2,17 +2,10 @@
 
 set -eu
 
+# It would have been nice to mount /dev as devtmpfs, but unfortunately
+# that's not possible if you're not privileged. For now tmpfs will
+# have to do.
 mount -n -t tmpfs none /dev
-# mount -t devtmpfs none /dev
-
-# mknod -m 622 /dev/console c 5 1
-# mknod -m 666 /dev/null c 1 3
-# mknod -m 666 /dev/zero c 1 5
-# mknod -m 666 /dev/ptmx c 5 2
-# mknod -m 666 /dev/tty c 5 0
-# mknod -m 444 /dev/random c 1 8
-# mknod -m 444 /dev/urandom c 1 9
-# chown root:tty /dev/{console,ptmx,tty}
 
 mount -t proc none /proc
 mount -t sysfs none /sys
@@ -21,11 +14,6 @@ mkdir -p /tmp.shadow
 mount --bind /tmp /tmp.shadow
 mount -t tmpfs none /tmp
 cp -a /tmp.shadow/* /tmp/
-
-# mount -t tmpfs none /etc
-
-# mkdir -p /dev/pts
-# mount -t devpts none /dev/pts
 
 mkdir -p "$FINIT_RCSD"
 
