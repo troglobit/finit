troglobit/finit

commit e2f6ac0ece574434641bc40933d8fb82161fb758

initctl: iterate command over all args, e.g. delete foo bar baz
Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

src/initctl.c
@@ -737,8 +737,6 @@ static int do_help(char *arg)
 
 int main(int argc, char *argv[])
 {
-	int interactive = 1, c;
-	char *cmd, arg[120];
 	struct command command[] = {
 		{ "debug",    toggle_debug },
 		{ "help",     do_help      },
@@ -783,6 +781,8 @@ int main(int argc, char *argv[])
 		{ "verbose",    0, NULL, 'v' },
 		{ NULL, 0, NULL, 0 }
 	};
+	int interactive = 1, c;
+	char *cmd;
 
 	if (transform(progname(argv[0])))
 		return reboot_main(argc, argv);
@@ -831,21 +831,23 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	memset(arg, 0, sizeof(arg));
 	cmd = argv[optind++];
-	while (optind < argc) {
-		strlcat(arg, argv[optind++], sizeof(arg));
-		if (optind < argc)
-			strlcat(arg, " ", sizeof(arg));
-	}
-
 	for (c = 0; command[c].cmd; c++) {
+		int rc = 0;
+
 		if (!string_match(command[c].cmd, cmd))
 			continue;
 		if (!command[c].cb)
 			continue;
 
-		return command[c].cb(arg);
+		/* no arg, just a command, e.g. initctl ls */
+		if (optind >= argc)
+			return command[c].cb(NULL);
+
+		/* >=1 arg, e.g., initctl del foo bar baz */
+		while (optind < argc)
+			rc |= command[c].cb(argv[optind++]);
+		return rc;
 	}
 
 	return usage(1);

commit 5da0a460fede27d7ce6ce574628602228dab8be3

initctl: fix runlevel and start/stop/restart segfault regression
Introduced in e2f6ac0.

Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

src/initctl.c
@@ -143,10 +143,9 @@ static int do_runlevel(char *arg)
 	struct init_request rq = {
 		.magic = INIT_MAGIC,
 		.cmd = INIT_CMD_RUNLVL,
-		.runlevel = (int)arg[0],
 	};
 
-	if (!rq.runlevel) {
+	if (!arg) {
 		int prevlevel = 0;
 		int runlevel;
 		char prev;
@@ -164,6 +163,9 @@ static int do_runlevel(char *arg)
 		printf("%c %d\n", prev , runlevel);
 		return 0;
 	}
+
+	/* set runlevel */
+	rq.runlevel = (int)arg[0];
 
 	return client_send(&rq, sizeof(rq));
 }
@@ -194,6 +196,13 @@ static int do_startstop(int cmd, char *arg)
 		.magic = INIT_MAGIC,
 		.cmd   = INIT_CMD_SVC_QUERY
 	};
+
+	if (!arg || !arg[0])
+		errx(1, "missing argument to %s", (cmd == INIT_CMD_START_SVC
+						   ? "start"
+						   : (cmd == INIT_CMD_STOP_SVC
+						      ? "stop"
+						      : "restart")));
 
 	strlcpy(rq.data, arg, sizeof(rq.data));
 	if (client_send(&rq, sizeof(rq))) {