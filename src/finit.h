50c99762f1ca7794618697ec4c4413df67db8799
Jacques de Laval <jacques@de-laval.se>
2021-03-05 01:39:42 +0100

    Add run target for running Finit as a stand alone unprivileged process
    It could be very useful, during development, to be able to run Finit on a
    host system without having to provision a full VM. We have all the building
    blocks for easily containerizing Finit - in the test framework - so adding
    a "run" target that does the orchestration makes sense.
    
    Signed-off-by: Jacques de Laval <jacques@de-laval.se>

--- src/finit.h
+++ src/finit.h
@@ -36,11 +36,6 @@
 #include <lite/lite.h>
 #include <uev/uev.h>
 
-/* just in case */
-#ifndef _PATH_STDPATH
-#define _PATH_STDPATH   "/usr/bin:/bin:/usr/sbin:/sbin"
-#endif
-
 #ifndef _PATH_VARRUN
 #define _PATH_VARRUN    "/var/run/"
 #endif
