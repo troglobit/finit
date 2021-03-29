troglobit/finit

commit 3bb1e41394fdea6ff2937b21bbabd53b8eb4a681

Only check for dangling symlinks, load enabled/*.conf file
In debug mode it's highly confusing to see Finit load available/*.conf
files when you expect it to only load finit.conf and enabled/*.conf,
so let's keep the dangling symlink check to itself and actually load
and parse from enabled/ as it was intended :)

Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

src/conf.c
@@ -677,7 +677,7 @@ int conf_reload(void)
 
 		/* Check for dangling symlinks */
 		if (S_ISLNK(st.st_mode)) {
-			path = rp = realpath(path, NULL);
+			rp = realpath(path, NULL);
 			if (!rp) {
 				logit(LOG_WARNING, "Skipping %s, dangling symlink: %s", path, strerror(errno));
 				continue;

commit bda4da71a68c2cf51765c0354f9d3163a90a0a41

Fix touch-reload regression from 3bb1e41
When reloading we check if service .conf files have been modified since
the last time we read them.  In 3bb1e41 we changed it so we load only
the enabled/*.conf files, which led to our no longer properly detecting
changed files since we monitor changes in what the symlinks point to.

This patch fixes that by checking the target for changes.

Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

src/conf.c
@@ -792,13 +792,21 @@ int conf_any_change(void)
 
 int conf_changed(char *file)
 {
+	int rc = 0;
+	char *rp;
+
 	if (!file)
 		return 0;
 
-	if (conf_find(file))
-		return 1;
+	rp = realpath(file, NULL);
+	if (!rp)
+		return 0;
 
-	return 0;
+	if (conf_find(rp))
+		rc = 1;
+	free(rp);
+
+	return rc;
 }
 
 static void conf_cb(uev_t *w, void *arg, int events)