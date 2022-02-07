troglobit/finit

commit f0f358a1388a7d1d505d58ef11bfa87d3ffb3490

service.c: set/clear condition 'done' for run tasks
We like to know when a runtask (run/task/sysv)is done, so as to start
the services that depending on it, set a oneshot condition 'done' for
that case and clear it when runtask services enter HALTED state.

Signed-off-by: Ming Liu <liu.ming50@gmail.com>

src/service.c
@@ -1677,6 +1677,21 @@ static void svc_set_state(svc_t *svc, svc_state_t new)
 		service_timeout_cancel(svc);
 		service_timeout_after(svc, svc->killdelay, service_kill);
 	}
+
+	if (svc_is_runtask(svc)) {
+		char cond[MAX_COND_LEN];
+
+		snprintf(cond, sizeof(cond), "%s/%s/done", svc->type, svc->name);
+
+		/* create done condition when entering SVC_DONE_STATE. */
+		if (*state == SVC_DONE_STATE)
+			cond_set_oneshot(cond);
+
+		/* clear done condition when entering SVC_HALTED_STATE. */
+		if (*state == SVC_HALTED_STATE)
+			cond_clear(cond);
+
+	}
 }
 
 /*

commit 885808c64cbe7c1f87a78f9ea2aed22de53c5721

service.c: fix a regression
A regression was introduced by commit f0f358a1:
[ service.c: set/clear condition 'done' for run tasks ]

svc->type is a integer but mistakenly being used as string, which will
cause crash.

Signed-off-by: Ming Liu <liu.ming50@gmail.com>

src/service.c
@@ -1709,7 +1709,7 @@ static void svc_set_state(svc_t *svc, svc_state_t new)
 	if (svc_is_runtask(svc)) {
 		char cond[MAX_COND_LEN];
 
-		snprintf(cond, sizeof(cond), "%s/%s/done", svc->type, svc->name);
+		snprintf(cond, sizeof(cond), "%s/%s/done", svc_typestr(svc), svc->name);
 
 		/* create done condition when entering SVC_DONE_STATE. */
 		if (*state == SVC_DONE_STATE)