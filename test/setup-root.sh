4a90a3e8efb94f58c66233398d2f6e0297ac3a37
Joachim Wiberg <troglobit@gmail.com>
2022-03-27 16:21:06 +0200

    test: drop plugins we don't need in test
    E.g., a failing urandom.so does no good for ppl reading test results.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- test/setup-root.sh
+++ test/setup-root.sh
@@ -8,5 +8,7 @@ make -C "$top_builddir" DESTDIR="$TENV_ROOT" install
 # shellcheck disable=SC2154
 FINITBIN="$(pwd)/$top_builddir/src/finit" DEST="$TENV_ROOT" make -f "$srcdir/tenv/root.mk"
 
-# Drop plugins we don't want to use in test, only causes FAIL in logs.
-rm -f "$TENV_ROOT/lib/finit/plugins/urandom.so"
+# Drop plugins we don't need in test, only causes confusing FAIL in logs.
+for plugin in tty.so urandom.so rtc.so modprobe.so hotplug.so; do
+	find "$TENV_ROOT" -name $plugin -delete
+done
