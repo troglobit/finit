troglobit/finit

commit 685f332c6e653ec446242db663b0169a8f079d7b

logit(): avoid extra \n when logging to stderr
Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

src/log.h
@@ -40,10 +40,10 @@
  * The default log level is LOG_NOTICE.  To toggle LOG_DEBUG messages,
  * use `initctl debug` or add `debug` to the kernel cmdline.
  */
-#define  _d(fmt, args...) logit(LOG_DEBUG,   "%s():" fmt "\n", __func__, ##args)
-#define  _w(fmt, args...) logit(LOG_WARNING, "%s():" fmt "\n", __func__, ##args)
-#define  _e(fmt, args...) logit(LOG_ERR,     "%s():" fmt "\n", __func__, ##args)
-#define _pe(fmt, args...) logit(LOG_ERR,     "%s():" fmt ": %s\n", __func__, ##args, strerror(errno))
+#define  _d(fmt, args...) logit(LOG_DEBUG,   "%s():" fmt, __func__, ##args)
+#define  _w(fmt, args...) logit(LOG_WARNING, "%s():" fmt, __func__, ##args)
+#define  _e(fmt, args...) logit(LOG_ERR,     "%s():" fmt, __func__, ##args)
+#define _pe(fmt, args...) logit(LOG_ERR,     "%s():" fmt ": %n", __func__, ##args, strerror(errno))
 
 void    log_init        (void);
 void    log_exit        (void);

src/log.c
@@ -116,6 +116,7 @@ void logit(int prio, const char *fmt, ...)
 
 	if (in_container() || !(fp = fopen("/dev/kmsg", "w"))) {
 		vfprintf(stderr, fmt, ap);
+		fputs("\n", stderr);
 		goto done;
 	}
 
@@ -127,6 +128,7 @@ void logit(int prio, const char *fmt, ...)
 		va_end(ap);
 		va_start(ap, fmt);
 		vfprintf(stderr, fmt, ap);
+		fputs("\n", stderr);
 	}
 
 done:

commit 1502838a0d88b3d95b52aba13e963df3ab399daa

Fix regression in _pe() log macro, introduced in 685f332
Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

src/log.h
@@ -43,7 +43,7 @@
 #define  _d(fmt, args...) logit(LOG_DEBUG,   "%s():" fmt, __func__, ##args)
 #define  _w(fmt, args...) logit(LOG_WARNING, "%s():" fmt, __func__, ##args)
 #define  _e(fmt, args...) logit(LOG_ERR,     "%s():" fmt, __func__, ##args)
-#define _pe(fmt, args...) logit(LOG_ERR,     "%s():" fmt ": %n", __func__, ##args, strerror(errno))
+#define _pe(fmt, args...) logit(LOG_ERR,     "%s():" fmt ": %s", __func__, ##args, strerror(errno))
 
 void    log_init        (void);
 void    log_exit        (void);