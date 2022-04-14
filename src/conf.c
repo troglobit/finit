troglobit/finit

commit ae09272bcc4fce879e8b57cb9b86bc1f54ef502b

logit(): detect running in container to use stderr for logging
When running in a container we still want to use any syslog daemon
available for our logging needs.  However, the time between the first
logit() in Finit and any such daemon having started can be long.  In a
normal (non-containerized) setup we log to the kernel ring buffer, but
that's not available in a container scenario.  At least not for
unprivileged containers.  So we need to detect all these cases and be
prepared to fall back to log to the console, either using these LOG_CONS
flag to openlog(), or by simply calling vfprintf() to stderr.

Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

src/log.c
@@ -59,18 +59,24 @@ void log_exit(void)
 	enable_progress(1);
 }
 
-static void log_open(void)
+static int log_open(void)
 {
-	int opts = LOG_PID;
+	int opts;
+
+	if (up)
+		return 1;
+	if (access("/dev/log", W_OK))
+		return 0;
+
+	opts = LOG_CONS | LOG_PID;
+	if (debug)
+		opts |= LOG_PERROR;
 
 	closelog();
-
-	if (debug)
-		opts |= LOG_PERROR; /* LOG_CONS | */
 	openlog("finit", opts, LOG_DAEMON);
 	setlogmask(LOG_UPTO(loglevel));
 
-	up = 1;
+	return up = 1;
 }
 
 /* Toggle debug mode */
@@ -95,15 +101,12 @@ void log_debug(void)
  */
 void logit(int prio, const char *fmt, ...)
 {
-	FILE *fp;
 	va_list ap;
+	FILE *fp;
 
 	va_start(ap, fmt);
 
-	if (up || fexist("/dev/log")) {
-		if (!up)
-			log_open();
-
+	if (up || log_open()) {
 		vsyslog(prio, fmt, ap);
 		goto done;
 	}
@@ -111,8 +114,7 @@ void logit(int prio, const char *fmt, ...)
 	if (LOG_PRI(prio) > loglevel)
 		goto done;
 
-	fp = fopen("/dev/kmsg", "w");
-	if (!fp) {
+	if (in_container() || !(fp = fopen("/dev/kmsg", "w"))) {
 		vfprintf(stderr, fmt, ap);
 		goto done;
 	}

commit 01cb088e51cae65d2e1cf2dd0b9f0afc1a49451d

Fix `initctl debug` toggle regression introduced in ae09272b
Calling `initctl debug` is supposed to toggle Finit debug messages on
the boot console.  This broke in ae09272b when improving support for
running Finit in containers.

Part of this change is a slight refactor of who calles log_init() when
starting up, and when to call ttinit().  We now call ttinit() every time
we toggle debug.

Also, toggling back to normal logging had a bug.  The new default log
level for Finit is LOG_INFO, but toggling back set it to LOG_NOTICE.

Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

src/conf.c
@@ -205,8 +205,6 @@ void conf_parse_cmdline(int argc, char *argv[])
 
 	parse_kernel_cmdline();
 	parse_kernel_loglevel();
-
-	log_init();
 }
 
 static int kmod_exists(char *mod)

src/finit.c
@@ -570,7 +570,7 @@ int main(int argc, char *argv[])
 	};
 	uev_ctx_t loop;
 
-	/* telinit or stand-alone process monitor */
+	/* user calling telinit or init */
 	if (getpid() != 1)
 		return telinit(argc, argv);
 
@@ -581,12 +581,12 @@ int main(int argc, char *argv[])
 
 	/*
 	 * Parse /proc/cmdline (debug, rescue, console=, etc.)
-	 * Also calls log_init() to set correct log level
 	 */
 	conf_parse_cmdline(argc, argv);
 
 	/*
-	 * Figure out system console(s)
+	 * Figure out system console(s) and call log_init() to set
+	 * correct log level, possibly finit.debug enabled.
 	 */
 	console_init();
 

src/helpers.c
@@ -81,7 +81,7 @@ char *console(void)
 
 void console_init(void)
 {
-	ttinit();
+	log_init();
 }
 
 ssize_t cprintf(const char *fmt, ...)

src/log.c
@@ -40,6 +40,8 @@ static int loglevel = LOG_INFO;
 
 void log_init(void)
 {
+	ttinit();
+
 	if (debug)
 		loglevel = LOG_DEBUG;
 	else
@@ -72,11 +74,16 @@ static int log_open(void)
 	if (debug)
 		opts |= LOG_PERROR;
 
-	closelog();
 	openlog("finit", opts, LOG_DAEMON);
 	setlogmask(LOG_UPTO(loglevel));
 
 	return up = 1;
+}
+
+static void log_close(void)
+{
+	closelog();
+	up = 0;
 }
 
 /* Toggle debug mode */
@@ -84,12 +91,8 @@ void log_debug(void)
 {
 	debug = !debug;
 
-	if (debug) {
-		loglevel = LOG_DEBUG;
-	} else {
-		loglevel = LOG_NOTICE;
-		ttinit();
-	}
+	log_close();
+	log_init();
 	log_open();
 
 	logit(LOG_NOTICE, "Debug mode %s", debug ? "enabled" : "disabled");