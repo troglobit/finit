4aae3403f58a6322d6940070e04f0f9424ecc6c5
Joachim Wiberg <troglobit@gmail.com>
2022-05-08 09:46:59 +0200

    test: new test, verify default runlevel from command line
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ test/runlevel.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Verify selecting alternate runlevel from cmdline
+set -eu
+
+TEST_DIR=$(dirname "$0")
+
+# shellcheck disable=SC2034
+FINIT_RUNLEVEL=9
+
+test_setup()
+{
+	say "Test start $(date)"
+}
+
+test_teardown()
+{
+	say "Test done $(date)"
+}
+
+# shellcheck source=/dev/null
+. "$TEST_DIR/tenv/lib.sh"
+
+say 'Check runlevel'
+lvl=$(texec sh -c "initctl runlevel | awk '{print \$2;}'")
+if [ "$lvl" -eq "9" ]; then
+	return 0
+fi
+
+return 1
