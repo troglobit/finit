troglobit/finit

commit d810ac00bcb1deea33724ca3c073a76bdb9001c2

Fix #302: add support for running scripts at shutdown
This change adds support for running hook scripts at two new hook points
during the shutdown process:

 - HOOK_SVC_DN: after all services and non-reserved processes have been
   killed (and collected)
 - HOOK_SYS_DN: after all file systems have been unmounted, *just prior*
   to Finit calling reboot() to shut down or reboot the system

See doc/plugins.md for details and an example.

Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

src/plugin.c
@@ -195,10 +195,39 @@ int plugin_exists(hook_point_t no)
 	return 0;
 }
 
+#ifdef HAVE_HOOK_SCRIPTS_PLUGIN
+#define CHOOSE(x, y) y
+static const char *hscript_paths[] = HOOK_TYPES;
+#undef CHOOSE
+
+void plugin_script_run(hook_point_t no)
+{
+	char path[CMD_SIZE] = "";
+
+	strlcat(path, PLUGIN_HOOK_SCRIPTS_PATH, sizeof(path));
+	strlcat(path, hscript_paths[no] + 4, sizeof(path));
+
+	run_parts(path, NULL);
+}
+#else
+void plugin_script_run(hook_point_t no)
+{
+	(void)no;
+}
+#endif
+
 /* Some hooks are called with a fixed argument */
 void plugin_run_hook(hook_point_t no, void *arg)
 {
 	plugin_t *p, *tmp;
+
+	if (!cond_is_available()) {
+#ifdef HAVE_HOOK_SCRIPTS_PLUGIN
+		warnx("conditions not available, calling script script based hooks only!");
+		plugin_script_run(no);
+#endif
+		return;
+	}
 
 	PLUGIN_ITERATOR(p, tmp) {
 		if (p->hook[no].cb) {

plugins/hook-scripts.c
@@ -26,18 +26,11 @@
 #include "plugin.h"
 #include "config.h"
 
-#define CHOOSE(x, y) y
-static const char *hscript_paths[] = HOOK_TYPES;
-#undef CHOOSE
+extern void plugin_script_run(hook_point_t no);
 
-static void hscript_run_parts(hook_point_t hook)
+static void hscript_run_parts(hook_point_t no)
 {
-	char path[CMD_SIZE] = "";
-
-	strlcat(path, PLUGIN_HOOK_SCRIPTS_PATH, sizeof(path));
-	strlcat(path, hscript_paths[hook] + 4, sizeof(path));
-
-	run_parts(path, NULL);
+	plugin_script_run(no);
 }
 
 static void hscript_banner(void *arg)
@@ -96,6 +89,14 @@ static plugin_t plugin = {
 	.hook[HOOK_SVC_UP]      = { .cb  = hscript_svc_up      },
 	.hook[HOOK_SYSTEM_UP]   = { .cb  = hscript_system_up   },
 	.hook[HOOK_SHUTDOWN]    = { .cb  = hscript_shutdown    },
+/*
+ * We have exited here, so these are handled a bit differently by Finit,
+ * when hook scripts are enabled, just to provide the user with the same
+ * interface:
+ *
+ *	.hook[HOOK_SVC_DN]      = { .cb  = hscript_svc_down    },
+ *	.hook[HOOK_SYS_DN]      = { .cb  = hscript_system_down },
+ */
 };
 
 PLUGIN_INIT(plugin_init)

src/plugin.h
@@ -82,6 +82,8 @@
 								\
 	/* Shutdown hooks, runlevel [06] */			\
 	CHOOSE(HOOK_SHUTDOWN,        "hook/sys/shutdown"),	\
+	CHOOSE(HOOK_SVC_DN,          "hook/svc/down"),		\
+	CHOOSE(HOOK_SYS_DN,          "hook/sys/down"),		\
 	CHOOSE(HOOK_MAX_NUM,         "nop")			\
 }
 

src/sig.c
@@ -329,6 +329,9 @@ void do_shutdown(shutop_t op)
 	while (waitpid(-1, NULL, WNOHANG) > 0)
 		;
 
+	/* All services and (non-critical) processes have stopped. */
+	plugin_run_hooks(HOOK_SVC_DN);
+
 	if (in_cont) {
 		if (osheading)
 			logit(LOG_CONSOLE | LOG_NOTICE, "%s, shutting down container.", osheading);
@@ -373,6 +376,9 @@ void do_shutdown(shutop_t op)
 
 	/* Call mdadm to mark any RAID array(s) as clean before halting. */
 	mdadm_wait();
+
+	/* Last chance to run scripts (all .so plugins have exited already) */
+	plugin_run_hooks(HOOK_SYS_DN);
 
 	/* Reboot via watchdog or kernel, or shutdown? */
 	if (op == SHUT_REBOOT) {

commit 729002ef82202c30b14d8137c6763a5fefc1a4b4

Fix regression introduced in d810ac0: drop 'return;' statement
The condition subsystem starts very late, this should not affect running
callbacks in plugins.  In d810ac0 an extra return statement was included
in a new if() statement block that run plugins hooks.  This caused a
massive regression in how Finit starts up and runs.

 - drop return; statement that causes regression
 - move #ifdefs to surround if() statement

Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

src/plugin.c
@@ -221,13 +221,12 @@ void plugin_run_hook(hook_point_t no, void *arg)
 {
 	plugin_t *p, *tmp;
 
-	if (!cond_is_available()) {
 #ifdef HAVE_HOOK_SCRIPTS_PLUGIN
-		warnx("conditions not available, calling script script based hooks only!");
+	if (!cond_is_available()) {
+		dbg("conditions not available, calling script script based hooks only!");
 		plugin_script_run(no);
-#endif
-		return;
 	}
+#endif
 
 	PLUGIN_ITERATOR(p, tmp) {
 		if (p->hook[no].cb) {