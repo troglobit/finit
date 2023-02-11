4f7e726a3e140e31d8a76ddd9b2af9093a145a45
Joachim Wiberg <troglobit@gmail.com>
2023-02-11 16:48:21 +0100

    test: initial regression test for #351 (WIP)
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ test/bootstrap-crash.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# Regression test for issue #351.  A run/task that never completes
+# successfully in runlevel S should not respawn forever.
+#set -eu
+
+TEST_DIR=$(dirname "$0")
+DEBUG=1
+BOOTSTRAP="run [S] crasher.sh -- Party crasher"
+
+# shellcheck source=/dev/null
+. "$TEST_DIR/lib/setup.sh"
+
+test_setup()
+{
+	say "Test start $(date)"
+}
+
+test_teardown()
+{
+	say "Test done $(date)"
+	say "Running test teardown."
+	run "rm -f $TEST_CONF"
+}
+
+say "Hello SMTP Spoken here"
+#sleep 1
+#run initctl
+#run initctl status crasher.sh
+
+
---
+++ test/skel/bin/crasher.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sleep 2
+kill -11 $$
+
+exit 1
+
