571250a9b98d6a924ca2ce704083e8dda881a4f2
Joachim Wiberg <troglobit@gmail.com>
2023-03-05 10:49:14 +0100

    test: drop hotplug.conf system file during tests
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- test/setup-sysroot.sh
+++ test/setup-sysroot.sh
@@ -15,3 +15,9 @@ FINITBIN="$(pwd)/$top_builddir/src/finit" DEST="$SYSROOT" make -f "$srcdir/lib/s
 for plugin in tty.so urandom.so rtc.so modprobe.so hotplug.so; do
 	find "$SYSROOT" -name $plugin -delete
 done
+
+# Drop system .conf files we don't need in test, same as above
+for conf in hotplug.conf; do
+	find "$SYSROOT" -name $conf -delete
+done
+
