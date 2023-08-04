b39a4498a347c9646b1fa7bb566b8df5855e161d
Joachim Wiberg <troglobit@gmail.com>
2023-08-04 22:04:13 +0200

    test: enable bootstrap-crash, regression test for #351
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- test/bootstrap-crash.sh
+++ test/bootstrap-crash.sh
@@ -4,7 +4,8 @@
 #set -eu
 
 TEST_DIR=$(dirname "$0")
-DEBUG=1
+#DEBUG=1
+# shellcheck disable=SC2034
 BOOTSTRAP="run [S] crasher.sh -- Party crasher"
 
 # shellcheck source=/dev/null
@@ -12,22 +13,17 @@ BOOTSTRAP="run [S] crasher.sh -- Party crasher"
 
 test_setup()
 {
-	say "Test start $(date)"
+    say "Test start $(date)"
 }
 
 test_teardown()
 {
-	say "Test done $(date)"
-	say "Running test teardown."
-	run "rm -f $TEST_CONF"
+    say "Test done $(date)"
 }
 
 say "Hello SMTP Spoken here"
+run initctl
+run initctl status crasher.sh
 
 sleep 10
 say "We are done"
-#run initctl
-#run initctl status crasher.sh
-
-
-exit 0
