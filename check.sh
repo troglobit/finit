e6da0a22e78eee208da2dea9b8cf857cd2013fc6
Joachim Wiberg <troglobit@gmail.com>
2023-09-10 12:05:10 +0200

    Helper script for development/test
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ check.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Run test suite with the correct configure params
+
+make distclean
+./configure --prefix=/usr --exec-prefix= --sysconfdir=/etc --localstatedir=/var \
+	    --enable-x11-common-plugin --with-watchdog --with-keventd
+make -j17 clean check
