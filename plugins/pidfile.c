66020f48ea36d44fe461677c8dfa639400b9f561
Joachim Wiberg <troglobit@gmail.com>
2023-11-13 04:37:03 +0100

    Do not create PID conditions for services that do not support it
    Follow-up to issue #386
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- plugins/pidfile.c
+++ plugins/pidfile.c
@@ -80,6 +80,10 @@ static void pidfile_update_conds(char *dir, char *name, uint32_t mask)
 	}
 
 	dbg("Found svc %s for %s with pid %d", svc_ident(svc, NULL, 0), fn, svc->pid);
+	if (svc->notify != SVC_NOTIFY_PID) {
+		dbg("svc %s does not use PID readiness notification, skipping.", svc_ident(svc, NULL, 0));
+		return;
+	}
 	mkcond(svc, cond, sizeof(cond));
 
 	if (mask & (IN_CLOSE_WRITE | IN_ATTRIB | IN_MODIFY | IN_MOVED_TO)) {
